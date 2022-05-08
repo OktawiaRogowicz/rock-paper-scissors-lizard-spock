88b46f83cc1cf09252259247a495ad624a6d85c2
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-05-08 23:21:39 +0200

    Prototype - added game mechanic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,186 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+import logo_bonus from "./rock-paper-scissors-master/images/logo-bonus.svg";
+import icon_rock from "./rock-paper-scissors-master/images/icon-rock.svg";
+import icon_paper from "./rock-paper-scissors-master/images/icon-paper.svg";
+import icon_scissors from "./rock-paper-scissors-master/images/icon-scissors.svg";
+import icon_spock from "./rock-paper-scissors-master/images/icon-spock.svg";
+import icon_lizard from "./rock-paper-scissors-master/images/icon-lizard.svg";
+import {useState} from "react";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+
+  box-sizing: border-box;
+  padding: 1.5rem;
+
+  background: radial-gradient(circle at top, hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+
+  overflow-y: hidden;
+  overflow-x: hidden;
+`
+
+const Header = styled.div`
+  width: 100%;
+  height: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 3px white solid;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 10px;
+
+  background: transparent;
+
+  img {
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Score = styled.div`
+  height: 100%;
+  min-width: 5rem;
+
+  box-sizing: border-box;
+  padding: 0.5rem;
+  margin: auto 0 auto auto;
+
+  background-color: white;
+  text-align: center;
+  font-size: 1rem;
+
+  span {
+    display: block;
+    font-size: 2rem;
+  }
+`
+
+const Rules = styled.button`
+  position: absolute;
+  background-color: transparent;
+  bottom: 3rem;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 2px white solid;
+  border-radius: 5px;
+  padding: 0.5rem 1rem 0.5rem 1rem;
+  font-size: 1rem;
+  color: white;
+  text-transform: uppercase;
+`
+
+const Pentagon = styled.div`
+  position: relative;
+  display: flex;
+  min-width: 100%;
+  background-color: powderblue;
+
+  #scissors {
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+  }
+
+  #spock {
+    top: 7.5rem;
+  }
+
+  #paper {
+    top: 7.5rem;
+    right: 0;
+  }
+
+  #lizard {
+    top: 15rem;
+    left: 2rem;
+  }
+
+  #rock {
+    top: 15rem;
+    right: 2rem;
+  }
+`
+
+const Icon = styled.button`
+  position: absolute;
+  min-height: 6rem;
+  min-width: 6rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border: 0.75rem gold solid;
+  border-radius: 50%;
+`
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [score, setScore] = useState(0);
+
+    function mod(n, m) {
+        return ((n % m) + m) % m;
+    }
+
+    var choiceEnum = {"spock": 0, "lizard": 1, "rock": 2, "paper": 3, "scissors": 4}
+
+    function compareChoices(usersChoice, computersChoice) {
+        let modulo = choiceEnum.scissors;
+        if (computersChoice === usersChoice) {
+            return 0;
+        } else if (
+            computersChoice === mod(usersChoice - 1, modulo) ||
+            computersChoice === mod(usersChoice - 3, modulo)) {
+            return -1;
+        }
+        return 1;
+    }
+
+    function onPickIcon(choice) {
+        let computersChoice = Math.floor(Math.random() * 5);
+        console.log("ai choice: ", computersChoice);
+        let result = compareChoices(choice, computersChoice);
+        console.log("result: ", result);
+        setScore(score + result);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <img src={logo_bonus} alt={"Logo"}/>
+                <Score>
+                    Score
+                    <span>{score}</span>
+                </Score>
+            </Header>
+            <Rules>Rules</Rules>
+
+            <Pentagon>
+                <Icon onClick={ () => onPickIcon(choiceEnum.scissors) } id="scissors"><img src={icon_scissors} alt={"Scissors"}/></Icon>
+                <Icon onClick={ () => onPickIcon(choiceEnum.spock) } id="spock"><img src={icon_spock} alt={"Spock"}/></Icon>
+                <Icon onClick={ () => onPickIcon(choiceEnum.paper) } id="paper"><img src={icon_paper} alt={"Paper"}/></Icon>
+                <Icon onClick={ () => onPickIcon(choiceEnum.lizard) } id="lizard"><img src={icon_lizard} alt={"Lizard"}/></Icon>
+                <Icon onClick={ () => onPickIcon(choiceEnum.rock) } id="rock"><img src={icon_rock} alt={"Rock"}/></Icon>
+            </Pentagon>
+
+
+            You Picked
+            The House Picked
+
+            You Win
+            You Lose
+
+            Play Again
+        </Container>
+    );
 }
 
 export default App;
